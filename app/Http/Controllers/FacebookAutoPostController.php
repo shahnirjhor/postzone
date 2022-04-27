fbb47ff6a2e3526febfee197e20ea394e44ac1a4
shahnirjhor <shah.nirjhor@gmail.com>
2022-04-27 22:46:50 +0600

    auto post start

---
+++ app/Http/Controllers/FacebookAutoPostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FacebookAutoPost;
+use Illuminate\Http\Request;
+
+class FacebookAutoPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FacebookAutoPost  $facebookAutoPost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FacebookAutoPost $facebookAutoPost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FacebookAutoPost  $facebookAutoPost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FacebookAutoPost $facebookAutoPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FacebookAutoPost  $facebookAutoPost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FacebookAutoPost $facebookAutoPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FacebookAutoPost  $facebookAutoPost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FacebookAutoPost $facebookAutoPost)
+    {
+        //
+    }
+}
